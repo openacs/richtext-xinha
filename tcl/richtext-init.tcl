86bd1488cf9d58606fa75254a67d42061c4fb0a0
Antonio Pisano <antonio@elettrotecnica.it>
2024-07-19 11:46:20 +0000

    Remove xowiki dependency from CKEditor4
    Rationale: CKEditor4 is EOL for its free version and the paid LTS version will be discontinued in December 2026.
    
    We now do not set an explicit default for the preferred richtext editor. Instead, the first richtext-* package that will be installed with check whether a preferred editor was chosen. If not, they will set themselves as the xowiki editor. This should not change the behavior for existing installations already using ckeditor4, or a different editor.

--- tcl/richtext-init.tcl
+++ tcl/richtext-init.tcl
@@ -1,5 +1,21 @@
 template::util::richtext::register_editor xinha
 
+if {[apm_package_installed_p xowiki]} {
+    #
+    # We become the preferred richtext editor for xowiki, if none was
+    # chosen so far.
+    #
+    set preferred_editor [::parameter::get_global_value \
+                              -package_key xowiki \
+                              -parameter PreferredRichtextEditor]
+    if {$preferred_editor eq ""} {
+        ::parameter::set_global_value \
+            -package_key xowiki \
+            -parameter PreferredRichtextEditor \
+            -value "xinha"
+    }
+}
+
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
